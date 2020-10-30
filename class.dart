35a8128eb879e6ee0e3a3deb23d88946e6daeb4e
Aloysious Benoy <aloysiousbenoy@gmail.com>
2020-10-30 22:47:03 +0530

    simple class

--- class.dart
+++ class.dart
@@ -1,8 +1,16 @@
-main(List <String> args)
-{class dino{
-int i;
-num blah;
+class Dino{
+String name;
+int age;
+Dino(){
+name ="NA";
+age = 0;}
+printDino(){
+print(this.age);
+print(this.name);
+}
+}
 
-} 
-
+main(List<String> args){
+Dino d = Dino();
+d.printDino();
 }
