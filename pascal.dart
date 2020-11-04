b39496942f4b0fbe066e1551faf5178350bf846f
Aloysious Benoy <aloysiousbenoy@gmail.com>
2020-11-05 01:10:50 +0530

    pascal working now

--- pascal.dart
+++ pascal.dart
@@ -2,10 +2,13 @@ import 'dart:io';
 
 main(List<String> args) {
   int n, i, j, k;
-  List<List<int>> a = [];
+  List<List<int>> a;
   print("Enter the no. of rows\n");
   n = int.parse(stdin.readLineSync());
+  a = List(n);
   for (i = 0; i < n; i++) {
+    a[i] = List<int>(i + 1);
+    // a[i].fillRange(0, i, 0);
     a[i][0] = 1;
     a[i][i] = 1;
     for (j = 1; j < i; j++) {
@@ -16,7 +19,7 @@ main(List<String> args) {
     for (k = i; k <= n; k++) {
       stdout.write(" ");
     }
-    for (j = 0; j <= i; j++) stdout.write("${a[i][j]}");
+    for (j = 0; j <= i; j++) stdout.write("${a[i][j]} ");
     print("\n");
   }
 }
