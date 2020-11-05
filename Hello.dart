1fac4d81594d44c4738a5df3bd2d5f342b83cd4f
Aloysious Benoy <aloysiousbenoy@gmail.com>
2020-11-05 23:08:53 +0530

    window size

--- Hello.dart
+++ Hello.dart
@@ -1,14 +1,15 @@
 import 'dart:io';
 
 main(List<String> args) {
-  String str;
-  double a, b;
-  print("Hello World");
-  print("Enter a string");
-  str = stdin.readLineSync();
-  print("Enter 2 no.");
-  a = double.parse(stdin.readLineSync());
-  b = double.parse(stdin.readLineSync());
-  print("Sum is ${(a / b).toStringAsPrecision(2)}");
-  print(str);
+  for (int j = 0; j < stdout.terminalLines - 1; j++) {
+    for (int i = 0; i < stdout.terminalColumns; i++) {
+      if (i == 0 || i == stdout.terminalColumns - 1)
+        stdout.write("|");
+      else if (j == 0 || j == stdout.terminalLines - 2)
+        stdout.write("-");
+      else
+        stdout.write(" ");
+    }
+    stdout.write("\n");
+  }
 }
